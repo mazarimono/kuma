39ac51e9155f221e83f43763ed6265141ecee822
S.K <45278831+BanquetKuma@users.noreply.github.com>
2019-05-20 23:01:58 +0900

    Add files via upload

---
+++ OBD-try.py
@@ -0,0 +1,43 @@
+import plotly_express as px
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+OBD2=pd.read_csv("https://raw.githubusercontent.com/BanquetKuma/OBD/master/OBD_GPS_CSV")
+
+col_options = [dict(label=x, value=x) for x in OBD2.columns]
+dimensions = ["x", "y", "color"]
+
+app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+server = app.server
+
+app.layout = html.Div(
+    [
+        html.H1("Visualization of OBD by Dash"),
+        html.Div(
+            [
+                html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
+                for d in dimensions
+            ],
+            style={"width": "25%", "float": "left"},
+        ),
+        dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"})
+    ])
+
+
+@app.callback(Output("graph", "figure"), [Input(d, "value") for d in dimensions])
+def make_figure(x, y, color):
+    return px.scatter(
+        OBD2,
+        x=x,
+        y=y,
+        color=color,
+        height=700)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
