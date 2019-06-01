eae7817c0a82c94cd29bec81059f2bf5cdae8857
hoga <hogawa098@gmail.com>
2019-06-01 12:45:13 +0900

    add server

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ col_options = [dict(label=x, value=x) for x in OBD2.columns]
 dimensions = ["x", "y", "color"]
 
 app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+server = app.server
 
 app.layout = html.Div(
     [
